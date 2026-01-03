a6487690e43374ffa2ed25d65579999be399c484
Sarthak Upadhyay <sarthak040624@gmail.com>
2026-01-03 14:59:36 +0000

    not aaplied

--- src/components/PixelCard.tsx
+++ src/components/PixelCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useRef, JSX } from 'react';
+import React, { useEffect, useRef, JSX, ReactNode, useLayoutEffect } from 'react';
 
 class Pixel {
   width: number;
